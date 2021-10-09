655568317f0c95c6bae967217c82cd7b5751045f
Samir Shneyder Diaz Nieto <sdiaz@Samir-Shneyder-Diaz-Nieto.local>
2021-10-09 12:25:33 -0500

    incoprando vistas

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import VistaTabla from './components/VistaTabla/VistaTabla';
+// import ListadoProductos from './components/ListadoProductos/ListadoProductos';
+// import VistaVenta from './components/VistaVentas/VistaVenta';
+
 // import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
@@ -33,6 +36,8 @@ ReactDOM.render(
     <Switch>
       < Route exact path="/" component={App}/> 
       < Route exact path="/ingreso" component={VistaTabla}/>
+      {/* < Route exact path="/ventas" component={VistaVenta}/>
+      < Route exact path="/listado-productos" component={ListadoProductos}/> */}
       < Route exact component={App}/>
     </Switch>
     </BrowserRouter>
