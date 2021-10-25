189ed68aa029ff7716e83807ae3a73b2d9a1a9ed
Samir Shneyder Diaz Nieto <sdiaz@Samir-Shneyder-Diaz-Nieto.local>
2021-10-24 23:36:38 -0500

    borrar

--- src/components/ListadoUsuarios/ListadoUsuarios.js
+++ src/components/ListadoUsuarios/ListadoUsuarios.js
@@ -188,65 +188,9 @@ const ListadoUsuarios = () => {
           />
         </div>
       </Container>
-      
 
-
-
-      <div className="list row">
-      <div className="col-md-8">
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search by title"
-            value={searchTitle}
-            onChange={onChangeSearchTitle}
-          />
-          <div className="input-group-append">
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              
-            >
-              Search
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-12 list">
-        <table
-          className="table table-striped table-bordered"
-          {...getTableProps()}
-        >
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>
-                    {column.render("Header")}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row, i) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
 		</>
 	);
-};
+}
 
 export default ListadoUsuarios;
