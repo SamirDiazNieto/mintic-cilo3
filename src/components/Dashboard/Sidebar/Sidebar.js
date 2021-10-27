0c35c7dea95f8262ca2158343ad7aa6e30a9f4d5
jeisone <jeisoneduardoep@ufps.edu.co>
2021-10-26 22:47:04 -0500

    pull

--- src/components/Dashboard/Sidebar/Sidebar.js
+++ src/components/Dashboard/Sidebar/Sidebar.js
@@ -4,12 +4,22 @@ import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as BiIcons from 'react-icons/bi';
-import { SidebarData } from './SidebarData';
+import  SidebarData  from './SidebarData';
 import SubMenu from './SubMenu';
 import { IconContext } from 'react-icons/lib';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { getAuth } from "firebase/auth";
 import { useHistory } from "react-router-dom";
+import ListadoUsuarios from '../../ListadoUsuarios/ListadoUsuarios';
+
+const data = [
+
+];
+
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const PATH_CUSTOMERS = process.env.REACT_APP_API_USUARIOS_PATH;
+const PATH_CUSTOMERS_USUARIOS = process.env.REACT_APP_API_USUARIOS_PATH;
+
 
 const Nav = styled.div`
 	background: #15171c;
@@ -49,12 +59,75 @@ const SidebarWrap = styled.div`
 `;
 
 const Sidebar = () => {
+	const sideBarD=SidebarData()
+	
 	const [sidebar, setSidebar] = useState(false);
 	const showSidebar = () => setSidebar(!sidebar);
 	const history = useHistory();
 	const auth = getAuth();
+	const [user] = useAuthState(auth);
+	const [errors, setErrors] = React.useState(null);
+	const [newVal, setNewVal] = React.useState(0);
+	var usuarioActivo=false
+	const [usuario, setUsuario] = React.useState({
+		data: data,
+		form: {
+		  // id: "",
+		  nombreUsuario: "",
+		  password: "",
+		  rol: "",
+		  estado:""
+		}
+	  });
 
 
+	React.useEffect(() => {
+		if (!user) return history.replace("/");
+		user.getIdToken(true).then(token => {
+		  // sessionStorage.setItem("token", token) 
+		  const requestOptions = {
+			method: 'GET',
+			headers: {
+			  'Content-Type': 'application/json',
+			  Authorization: `Bearer ${token}`,
+			},
+		  };
+		  fetch(`${BASE_URL}${PATH_CUSTOMERS}`, requestOptions)
+			.then(res => res.json())
+			.then(
+			  (result) => {
+				//setIsLoaded(true);
+				setUsuario({
+				  ...usuario,
+				  data: result
+				});
+				//console.log("user.email")
+				//console.log(user.email)
+				//console.log("usuario")
+				//console.log(usuario)
+	
+				usuario.data.map((us)=>{
+					//console.log("validar")
+					if (us.nombreUsuario===user.email){
+						//console.log(us.rol)
+						//console.log(us.estado)
+						if(us.estado==="Pendiente"){
+							//console.log("usuarioActivo")
+							//console.log(usuarioActivo)
+							//usuarioActivo=true
+							//console.log(usuarioActivo)
+						}
+					}
+				})
+			  },
+			  (error) => {
+				//setIsLoaded(true);
+				setErrors(error);
+			  }
+			)
+		});
+	  }, [newVal]);
+	
 	const logout = () => {
 		auth.signOut().then(function () {
 		  console.log("loggedout");
@@ -80,8 +153,13 @@ const Sidebar = () => {
 						<NavIcon to='#'>
 							<AiIcons.AiOutlineClose onClick={showSidebar} />
 						</NavIcon>
-						{SidebarData.map((item, index) => {
-							return <SubMenu item={item} key={index} />;
+						{sideBarD.map((item, index) => {
+							
+							
+							return <SubMenu disabled={true} item={item} key={index} />;
+							
+							
+							
 						})}
 					</SidebarWrap>
 				</SidebarNav>
--- src/components/Dashboard/Sidebar/SidebarData.js
+++ src/components/Dashboard/Sidebar/SidebarData.js
@@ -4,93 +4,175 @@ import * as AiIcons from 'react-icons/ai';
 import * as MdIcons from 'react-icons/md';
 //import * as RiIcons from 'react-icons/ri';
 import * as BsIcons from 'react-icons/bs';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
+import { useHistory } from "react-router-dom";
 
-export const SidebarData = [
-	{
-		titulo: 'Inicio',
-		ruta: '/dashboard',
-		icono: <AiIcons.AiFillHome />,
-	},
-	{
-		titulo: 'Productos',
-		ruta: '/dashboard/lista-productos',
-		icono: <FaIcons.FaShoppingCart />,
-		// iconoCerrado: <RiIcons.RiArrowDownSFill />,
-		// iconoAbierto: <RiIcons.RiArrowUpSFill />,
-		// subNav: [
-		// 	{
-		// 		titulo: 'Registrar',
-		// 		ruta: '/dashboard/productos/registrar',
-		// 		icono: <AiIcons.AiOutlineAppstoreAdd />,
-		// 	},
-		// 	{
-		// 		titulo: 'Listar',
-		// 		ruta: '/dashboard/productos/listar',
-		// 		icono: <FaIcons.FaListOl />,
-		// 	},
-		// 	{
-		// 		titulo: 'Buscar',
-		// 		ruta: '/dashboard/productos/buscar',
-		// 		icono: <AiIcons.AiOutlineFileSearch />,
-		// 	},
-		// 	{
-		// 		titulo: 'Actualizar',
-		// 		ruta: '/dashboard/productos/actualizar',
-		// 		icono: <FaIcons.FaEdit />,
-		// 	},
-		// ],
-	},
-	{
-		titulo: 'Ventas',
-		ruta: '/dashboard/lista-ventas',
-		icono: <BsIcons.BsReceiptCutoff />,
-		// iconoCerrado: <RiIcons.RiArrowDownSFill />,
-		// iconoAbierto: <RiIcons.RiArrowUpSFill />,
-		// subNav: [
-		// 	{
-		// 		titulo: 'Registrar',
-		// 		ruta: '/dashboard/ventas/registrar',
-		// 		icono: <AiIcons.AiOutlineAppstoreAdd />,
-		// 	},
-		// 	{
-		// 		titulo: 'Listar',
-		// 		ruta: '/dashboard/ventas/listar',
-		// 		icono: <FaIcons.FaListOl />,
-		// 	},
-		// 	{
-		// 		titulo: 'Buscar',
-		// 		ruta: '/dashboard/ventas/buscar',
-		// 		icono: <AiIcons.AiOutlineFileSearch />,
-		// 	},
-		// 	{
-		// 		titulo: 'Actualizar',
-		// 		ruta: '/dashboard/ventas/actualizar',
-		// 		icono: <FaIcons.FaEdit />,
-		// 	},
-		// ],
-	},
-	{
-		titulo: 'Usuarios',
-		ruta: '/dashboard/lista-usuarios',
-		icono: <FaIcons.FaUserCog />,
-		// iconoCerrado: <RiIcons.RiArrowDownSFill />,
-		// iconoAbierto: <RiIcons.RiArrowUpSFill />,
-		// subNav: [
-		// 	{
-		// 		titulo: 'Listar',
-		// 		ruta: '/dashboard/usuarios/listar',
-		// 		icono: <FaIcons.FaListOl />,
-		// 	},
-		// 	{
-		// 		titulo: 'Actualizar',
-		// 		ruta: '/dashboard/usuarios/actualizar',
-		// 		icono: <FaIcons.FaEdit />,
-		// 	},
-		// ],
-	},
-	{
-		titulo: 'Soporte',
-		ruta: '/dashboard/soporte',
-		icono: <MdIcons.MdOutlineContactSupport />,
-	},
+const data = [
+
 ];
+
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const PATH_CUSTOMERS = process.env.REACT_APP_API_USUARIOS_PATH;
+const PATH_CUSTOMERS_USUARIOS = process.env.REACT_APP_API_USUARIOS_PATH;
+
+const SidebarData = () => {
+		const history = useHistory();
+		const auth = getAuth();
+		const [user] = useAuthState(auth);
+		const [errors, setErrors] = React.useState(null);
+		const [newVal, setNewVal] = React.useState(0);
+		var usuarioActivo=false
+		const [us, setUs] = React.useState({
+			data: data,
+			form: {
+			  // id: "",
+			  nombreUsuario: "",
+			  password: "",
+			  rol: "",
+			  estado:""
+			}
+		  });
+		  React.useEffect(() => {
+			if (!user) return history.replace("/");
+			user.getIdToken(true).then(token => {
+			  // sessionStorage.setItem("token", token) 
+			  const requestOptions = {
+				method: 'GET',
+				headers: {
+				  'Content-Type': 'application/json',
+				  Authorization: `Bearer ${token}`,
+				},
+			  };
+			  fetch(`${BASE_URL}${PATH_CUSTOMERS}`, requestOptions)
+				.then(res => res.json())
+				.then(
+				  (result) => {
+					  console.log(result)
+					//setIsLoaded(true);
+					
+
+					console.log("user.email---")
+				console.log(user.email)
+				console.log("usuario----")
+				result.map((us)=>{
+					console.log("validar-----")
+					if (us.nombreUsuario===user.email){
+						console.log(us.rol)
+						console.log(us.estado)
+						if(us.estado==="Pendiente"){
+							console.log("usuarioActivo-----")
+							console.log(usuarioActivo)
+							usuarioActivo=true
+							console.log(usuarioActivo)}
+							console.log("sidebarDATA")
+							console.log(sidebarDATA)
+					}
+				})
+					
+				
+		
+					
+				  },
+				  (error) => {
+					//setIsLoaded(true);
+					setErrors(error);
+				  }
+				)
+				
+			});
+		  }, [newVal]);
+		  const sidebarDATA=[
+			{
+				titulo: 'Inicio',
+				ruta: '/dashboard',
+				icono: <AiIcons.AiFillHome />,
+			},
+			{
+				titulo: 'Productos',
+				ruta: '/dashboard/lista-productos',
+				icono: <FaIcons.FaShoppingCart />,
+				// iconoCerrado: <RiIcons.RiArrowDownSFill />,
+				// iconoAbierto: <RiIcons.RiArrowUpSFill />,
+				// subNav: [
+				// 	{
+				// 		titulo: 'Registrar',
+				// 		ruta: '/dashboard/productos/registrar',
+				// 		icono: <AiIcons.AiOutlineAppstoreAdd />,
+				// 	},
+				// 	{
+				// 		titulo: 'Listar',
+				// 		ruta: '/dashboard/productos/listar',
+				// 		icono: <FaIcons.FaListOl />,
+				// 	},
+				// 	{
+				// 		titulo: 'Buscar',
+				// 		ruta: '/dashboard/productos/buscar',
+				// 		icono: <AiIcons.AiOutlineFileSearch />,
+				// 	},
+				// 	{
+				// 		titulo: 'Actualizar',
+				// 		ruta: '/dashboard/productos/actualizar',
+				// 		icono: <FaIcons.FaEdit />,
+				// 	},
+				// ],
+			},
+			{
+				titulo: 'Ventas',
+				ruta: '/dashboard/lista-ventas',
+				icono: <BsIcons.BsReceiptCutoff />,
+				// iconoCerrado: <RiIcons.RiArrowDownSFill />,
+				// iconoAbierto: <RiIcons.RiArrowUpSFill />,
+				// subNav: [
+				// 	{
+				// 		titulo: 'Registrar',
+				// 		ruta: '/dashboard/ventas/registrar',
+				// 		icono: <AiIcons.AiOutlineAppstoreAdd />,
+				// 	},
+				// 	{
+				// 		titulo: 'Listar',
+				// 		ruta: '/dashboard/ventas/listar',
+				// 		icono: <FaIcons.FaListOl />,
+				// 	},
+				// 	{
+				// 		titulo: 'Buscar',
+				// 		ruta: '/dashboard/ventas/buscar',
+				// 		icono: <AiIcons.AiOutlineFileSearch />,
+				// 	},
+				// 	{
+				// 		titulo: 'Actualizar',
+				// 		ruta: '/dashboard/ventas/actualizar',
+				// 		icono: <FaIcons.FaEdit />,
+				// 	},
+				// ],
+			},
+			{
+				titulo: 'Usuarios',
+				ruta: '/dashboard/lista-usuarios',
+				icono: <FaIcons.FaUserCog />,
+				// iconoCerrado: <RiIcons.RiArrowDownSFill />,
+				// iconoAbierto: <RiIcons.RiArrowUpSFill />,
+				// subNav: [
+				// 	{
+				// 		titulo: 'Listar',
+				// 		ruta: '/dashboard/usuarios/listar',
+				// 		icono: <FaIcons.FaListOl />,
+				// 	},
+				// 	{
+				// 		titulo: 'Actualizar',
+				// 		ruta: '/dashboard/usuarios/actualizar',
+				// 		icono: <FaIcons.FaEdit />,
+				// 	},
+				// ],
+			},
+			{
+				titulo: 'Soporte',
+				ruta: '/dashboard/soporte',
+				icono: <MdIcons.MdOutlineContactSupport />,
+			},
+		];
+return( sidebarDATA)
+	  
+	}
+
+	  export default SidebarData;
